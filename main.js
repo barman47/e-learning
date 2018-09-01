9a8dece9fc16fb01e58b0d35480fd60fd00224b3
barman47 <nomsouzoanya@yahoo.co.uk>
2018-09-01 01:08:17 +0100

    Added homepage icons

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const favicon = require('express-favicon');
 const path =  require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3200;
 const publicPath = path.join(__dirname, 'public');
 
+app.use(favicon(publicPath + '/img/favicon.png'));
 app.use(express.static(publicPath));
 app.engine('.hbs', exphbs({
     extname: '.hbs',
@@ -15,7 +17,7 @@ app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
     res.render('home', {
-        title: 'Home',
+        title: 'Edufy - Home',
         style: 'css/index.css',
         script: 'js/home.js'
     });
--- public/js/home.js
+++ public/js/home.js
@@ -1,25 +1,32 @@
 $(document).ready(function () {
 
-        $('.pushpin').pushpin({
-            top: $('.pushpin').offset().top,
-            
-        });
-        $('.sidenav').sidenav();
-        $('.dropdown-trigger').dropdown({hover: true});
-        $('.parallax').parallax();
-        $('.collapsible').collapsible();
-//     window.onscroll = function () {
-//         stick();
-//     }
+    $('.pushpin').pushpin({
+        top: $('.pushpin').offset().top,            
+    });
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown({hover: true});
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.scrollspy').scrollSpy();
+    $('#top').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+    
+            var hash = this.hash;
+    
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 
-//     var navbar = document.getElementById('menu');
-//     var sticky = navbar.offsetTop;
-
-//     function stick () {
-//         if (window.pageYOffset >= sticky) {
-//             navbar.classList.add('sticky');
-//         } else {
-//             navbar.classList.remove('sticky');
-//         }
-//     } 
+    window.onscroll = function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById('top').style.display = 'block';
+        } else {
+            document.getElementById('top').style.display = 'none';	
+        }
+    };
 });
