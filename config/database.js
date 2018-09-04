8fa766e916abe9711dce95e7bb2dfb46bb1613f0
barman47 <nomsouzoanya@yahoo.co.uk>
2018-09-04 16:03:17 +0100

    Added login pages

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/elearning',
-    // database: 'mongodb://barman:VICEcity47@ds113122.mlab.com:13122/logbookapp',
+    // database: 'mongodb://localhost:27017/elearning',
+    database: 'mongodb://barman:VICEcity47@ds141972.mlab.com:41972/elearning',
     secret: 'barman secret'
 };
